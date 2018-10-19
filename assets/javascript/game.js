12f7425c5685f9830710415fe8578f32dffef630
Vanya Levchenko <vanyalevchenko@Vanyas-MacBook-Pro.local>
2018-10-19 00:06:00 -0600

    changes made

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,10 +6,12 @@ var rightWord = [];
 var wrongWord = [];
 var chosenWord = words[randWord];
 var underScore = [];
+var docUnderScore = document.getElementsByClassName("underscore");
+var docRightGuess = document.getElementsByClassName("rightGuess");
+var docWrongGuess = document.getElementsByClassName("wrongGuess");
 // Testing
 console.log(chosenWord);
-// var docUnderScore = document.getElementsByClassName("underscore");
-// var docRightGuess = document.getElementsByClassName("RightGuess");
+
 // create underscores based on word length
 let generateUnderscore = () => {
     for(let i = 0; i < chosenWord.length; i++){
@@ -27,20 +29,29 @@ document.addEventListener("keypress",(event) => {
     if(chosenWord.indexOf(keyword) > -1) {
         // if guess is right
         rightWord.push(keyword);
+        docUnderScore[0].innerHTML = underScore.join("");
         // Replace underscore with letter
         underScore[chosenWord.indexOf(keyword)] = keyword;
-        if(underScore.join('') == chosenWord) {
+        docRightGuess[0].innerHTML = rightWord.join("");
+    if(underScore.join('') == chosenWord) {
             alert('YOU WIN!');
         }
         // testing
+        console.log(chosenWord);
         console.log(rightWord);
 }
 else {
     // if guess is wrong
     wrongWord.push(keyword);
+    docWrongGuess[0].innerHTML = wrongWord.join("");
+if(docWrongGuess > 5){
+    alert("YOU LOSE!");
+}
+}
+    
     // testing
     console.log(wrongWord);
-}
+
     // Testing
     console.log(event);
     console.log(keycode);
